f0199c8f02db9dd8bdc5c137d5bd5abb402d70e3
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-05-30 18:56:12 +0600

    route protection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,37 @@ import { Home } from "./pages/Home/Home"
 import SignIn from "./pages/SignIn/SignIn"
 import SignUp from "./pages/SignUp/SignUp"
 import Navbar from "./components/Navbar/Navbar"
+import ProtectedRoute from "./Protection/ProtectedRoute"
+
 const App = () => {
     return (
         <Router>
             <Navbar />
             <Routes>
-                <Route path='/dashboard' element={<Home />} />
-                <Route path='/signin' element={<SignIn />} />
-                <Route path='/signup' element={<SignUp />} />
+                <Route
+                    path='/dashboard'
+                    element={
+                        <ProtectedRoute accessBy='authorized'>
+                            <Home />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path='/signin'
+                    element={
+                        <ProtectedRoute accessBy='unauthorized'>
+                            <SignIn />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path='/signup'
+                    element={
+                        <ProtectedRoute accessBy='unauthorized'>
+                            <SignUp />
+                        </ProtectedRoute>
+                    }
+                />
             </Routes>
         </Router>
     )
